02e1091f9958d98ce85efe20df84d12fd0c19729
Rai <andre.wandeler@me.com>
2026-02-04 08:41:17 -0800

    Fix Plotting Data Read

--- LabsCode/MATLAB/lab02_graphing.m
+++ LabsCode/MATLAB/lab02_graphing.m
@@ -4,49 +4,15 @@
 
 clear;
 
-readfile = false; %% TODO update when in lab to be true
+readfile = true; %% TODO update when in lab to be true
 
 if readfile
-    filenum = '008'; % file number for the data you want to read
-    infofile = strcat('INF', filenum, '.TXT');
-    datafile = strcat('LOG', filenum, '.BIN');
-
-    % map from datatype to length in bytes
-    dataSizes.('float') = 4;
-    dataSizes.('ulong') = 4;
-    dataSizes.('int') = 4;
-    dataSizes.('int32') = 4;
-    dataSizes.('uint8') = 1;
-    dataSizes.('uint16') = 2;
-    dataSizes.('char') = 1;
-    dataSizes.('bool') = 1;
-
-    % read from info file to get log file structure
-    fileID = fopen(infofile);
-    items = textscan(fileID,'%s','Delimiter',',','EndOfLine','\r\n');
-    fclose(fileID);
-    [ncols,~] = size(items{1});
-    ncols = ncols/2;
-    varNames = items{1}(1:ncols)';
-    varTypes = items{1}(ncols+1:end)';
-    varLengths = zeros(size(varTypes));
-    colLength = 256;
-    for i = 1:numel(varTypes)
-        varLengths(i) = dataSizes.(varTypes{i});
-    end
-    R = cell(1,numel(varNames));
-
-    % read column-by-column from datafile
-    fid = fopen(datafile,'rb');
-    for i=1:numel(varTypes)
-        %# seek to the first field of the first record
-        fseek(fid, sum(varLengths(1:i-1)), 'bof');
-
-        %# % read column with specified format, skipping required number of bytes
-        R{i} = fread(fid, Inf, ['*' varTypes{i}], colLength-varLengths(i));
-        eval(strcat(varNames{i},'=','R{',num2str(i),'};'));
-    end
-    fclose(fid);
+    load("10and20.mat"); % variable is teensyanalog
+    arrsize = size(teensyanalog);
+    disp(arrsize(1));
+    samples = 1:arrsize(1);
+    teensyanalog = double(teensyanalog);
+    teensyanalog = teensyanalog';
 else
     % For testing with sample data only
     xtesting = 0:1000;
@@ -56,6 +22,9 @@ else
     ytesting = ytesting+10*noise; % initial noise
     ytesting(ytesting<0) = 0;
     ytesting = ytesting+noise; % add extra noise arund 0v
+
+    teensyanalog = ytesting;
+    samples = xtesting;
 end
 
 %% Data Processing
@@ -67,18 +36,18 @@ end
 f = figure;
 hold on
 
-plot(xtesting, ytesting, '.', 'MarkerSize',1)
+plot(samples, teensyanalog, '.', 'MarkerSize',1)
 
-yu = max(ytesting);
-yl = min(ytesting);
+yu = max(teensyanalog);
+yl = min(teensyanalog);
 yr = abs((yu-yl));
-ym = mean(ytesting);
-[pks, locs] = findpeaks(ytesting,xtesting,'MinPeakHeight',yu*0.95,'MinPeakProminence',yr/5); % find sin wave peaks of measured function
+ym = mean(teensyanalog);
+[pks, locs] = findpeaks(teensyanalog,samples,'MinPeakHeight',yu*0.95,'MinPeakProminence',yr/5); % find sin wave peaks of measured function
 period_samples = diff(locs);
 predicted_period = mean(period_samples);
 model = fittype('a*sin(b*x+c)+d',dependent="y",independent="x",coefficients=["a" "b" "c" "d"]);
-excludedPoints = find(ytesting <= yl+(0.01*yr)); % exclude all points within 1% of zero from fit
-fittedmodel = fit(xtesting',ytesting',model,'start',[yr/2,(2*pi)/predicted_period,rand(),ym],'Exclude', excludedPoints);
+excludedPoints = find(teensyanalog <= yl+(0.01*yr)); % exclude all points within 1% of zero from fit
+fittedmodel = fit(samples',teensyanalog',model,'start',[yr/2,(2*pi)/predicted_period,rand()*100,ym],'Exclude', excludedPoints);
 plot(fittedmodel)
 model_coeffs = coeffvalues(fittedmodel);
 legend("Teensy ADC Measurement", "Predicted Voltage Signal")
@@ -97,8 +66,8 @@ hold on
 xlabel("Time [s]")
 ylabel("Voltage [V]")
 
-time = (xtesting/predicted_period)*(1/signal_frequency);
-voltage = (ytesting/(2*model_coeffs(1)))*signal_amplitude;
+time = (samples/predicted_period)*(1/signal_frequency);
+voltage = (teensyanalog/(2*model_coeffs(1)))*signal_amplitude;
 
 plot(time, voltage, '.', 'MarkerSize',1)
 
--- LabsCode/MATLAB/matlablogging.m
+++ LabsCode/MATLAB/matlablogging.m
@@ -3,7 +3,7 @@
 
 function teensyanalog=matlablogging(length)
     length = 5000;  % 5000 is hardcoded buffer size on Teensy
-    s = serial('COM7','BaudRate',115200); % TODO edit serial port once teensy is plugged in
+    s = serial('/dev/tty.usbmodem175850901','BaudRate',115200); % TODO edit serial port once teensy is plugged in
     set(s,'InputBufferSize',2*length)
     fopen(s);
     fprintf(s,'%d',2*length)         % Send length to Teensy
@@ -11,7 +11,7 @@ function teensyanalog=matlablogging(length)
     fclose(s);
     teensyanalog = uint8(dat);
     teensyanalog = typecast(teensyanalog,'uint16');
-    save('teensyanalogdata.mat') % added this for redundancy backup
+    save('10mand20m.mat') % added this for redundancy backup
 end
 
 
