05dd54c47daf103c4f55a913e078e88673562946
Brave Yongphiphatwong <byongphiphatwong@g.hmc.edu>
2026-04-17 14:41:19 -0700

    Update TimingOffsets.h

--- FinalRobotCode/libraries/main/TimingOffsets.h
+++ FinalRobotCode/libraries/main/TimingOffsets.h
@@ -3,7 +3,7 @@
 
 // all times are recorded in units of ms
 // the offsets define when tasks happen within a loop period
-#define LOOP_PERIOD 99
+#define LOOP_PERIOD 100
 #define PRINTER_LOOP_OFFSET 0
 #define IMU_LOOP_OFFSET 10
 #define GPS_LOOP_OFFSET 20
